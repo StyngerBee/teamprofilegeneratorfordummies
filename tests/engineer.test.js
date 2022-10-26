3966f91e146fafe3b5fa3148d4271f917a9ca0dd
Curran Duke <cdsonics@yahoo.com>
2022-10-26 13:57:02 -0400

    added comments to engineer.test

--- tests/engineer.test.js
+++ tests/engineer.test.js
@@ -1,7 +1,9 @@
+// pulls in engineer class from classes file
 const Engineer = require('../lib/engineer.classes');
 
+// tests a whole suite
 describe('Engineer', () => {
-    
+    //begins individual test for methods, getRole
     describe('getRole', () => {
         it('should return employee role', () => {
             const role = 'Engineer';
@@ -11,7 +13,7 @@ describe('Engineer', () => {
             expect(role).toEqual(newRole);
         });
     });
-    
+    //begins individual test for methods, getGithub
     describe('getGithub', () => {
         it('should return employee github', () => {
             const github = 'StyngerBee';
