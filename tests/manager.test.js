0f3dd73025240a0365033c23c555e67a361e1e74
Curran Duke <cdsonics@yahoo.com>
2022-10-26 13:59:47 -0400

    adds comments for manager.test

--- tests/manager.test.js
+++ tests/manager.test.js
@@ -1,7 +1,10 @@
+// pulls in engineer class from classes file
 const Manager = require('../lib/manager.classes');
 
+// tests a whole suite
 describe('Manager', () => {
     
+    //begins individual test for methods, getRole
     describe('getRole', () => {
         it('should return employee role', () => {
             const role = 'Manager';
